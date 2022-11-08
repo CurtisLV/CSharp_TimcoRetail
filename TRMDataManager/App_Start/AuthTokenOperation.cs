f220e58a283f2e6b27b5c1f609f7a150f7fd5f7c
CurtisLV <curtislv@gmail.com>
2022-11-08 12:01:02 +0200

    AuthTokenOperation class 1

--- TRMDataManager/App_Start/AuthTokenOperation.cs
+++ TRMDataManager/App_Start/AuthTokenOperation.cs
@@ -11,7 +11,23 @@ namespace TRMDataManager.App_Start
     {
         public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
         {
-            throw new NotImplementedException();
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation()
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string> { "application/x-www-form-urlencoded" },
+                    parameters = new List<Parameter> { new Parameter()
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+            
         }
     }
 }
