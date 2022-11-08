093140c33da6dbf57164b29721da614457a30ecb
CurtisLV <curtislv@gmail.com>
2022-11-08 12:15:54 +0200

    AuthorizationOperationFilter 1

--- TRMDataManager/App_Start/AuthorizationOperationFilter.cs
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
 
 namespace TRMDataManager.App_Start
 {
-    public class AuthorizationOperationFilter
+    public class AuthorizationOperationFilter : IOperationFilter
     {
-        //
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
