0cfb45735690950cbdf6adf3395e1ec0c3fab7c4
CurtisLV <curtislv@gmail.com>
2022-11-14 21:18:24 +0200

    Sale table 3

--- TRMData/dbo/Tables/Sale.sql
+++ TRMData/dbo/Tables/Sale.sql
@@ -2,8 +2,8 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
 	[CashierId] NVARCHAR(128) NOT NULL, 
-    [SaleDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-    [SubTotal] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL, 
-    [Total] MONEY NOT NULL
+	[SaleDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+	[SubTotal] MONEY NOT NULL, 
+	[Tax] MONEY NOT NULL, 
+	[Total] MONEY NOT NULL
 )
