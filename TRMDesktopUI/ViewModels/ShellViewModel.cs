089d179ac348d1dbff34413655b749fdfd3840ad
CurtisLV <curtislv@gmail.com>
2022-11-15 10:50:52 +0200

    adding Caliburn.Micro

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace TRMDesktopUI.ViewModels
 {
-    internal class ShellViewModel
+    internal class ShellViewModel : Conductor<object>
     {
         //
     }
