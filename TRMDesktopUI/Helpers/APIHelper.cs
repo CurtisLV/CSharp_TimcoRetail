7449a7ddeafe712bd6df9892eb65fefc2f57b6a0
CurtisLV <curtislv@gmail.com>
2022-11-16 21:19:03 +0200

    APIHelper 2

--- TRMDesktopUI/Helpers/APIHelper.cs
+++ TRMDesktopUI/Helpers/APIHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,12 @@ namespace TRMDesktopUI.Helpers
 {
     public class APIHelper
     {
-        //
+        public HttpClient ApiClient { get; set; }
+
+        private void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+        }
     }
 }
