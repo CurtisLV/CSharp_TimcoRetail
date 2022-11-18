440b1ef537fdba6f49dae1c098d2220972ed3c8e
CurtisLV <curtislv@gmail.com>
2022-11-18 12:06:13 +0200

    SqlDataAccess 3

--- TRMDataManager.Library/SqlDataAccess.cs
+++ TRMDataManager.Library/SqlDataAccess.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,19 @@ namespace TRMDataManager.Library
         public string GetConnectionString(string name)
         {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(
+            string storedProcedure,
+            U parameters,
+            string connectionStringName
+        )
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                //
+            }
         }
     }
 }
