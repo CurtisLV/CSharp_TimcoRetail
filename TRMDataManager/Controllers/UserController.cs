360bdc3b1bdbeed91accfc8e76186eb574c646cd
CurtisLV <curtislv@gmail.com>
2022-11-19 10:39:17 +0200

    UserController 4

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Internal.Models;
 
 namespace TRMDataManager.Controllers
 {
     [Authorize]
     [RoutePrefix("api/User")]
-    public class UserController : Controller
+    public class UserController : ApiController
     {
-        // GET: User/Details/5
-        public ActionResult Details(int id)
+        public string Get(int id)
         {
-            return View();
+            return "value";
+        }
+
+        // GET: User/Details/5
+        public List<UserModel> GetById(string id)
+        {
+            UserData data = new UserData();
+
+            return data.GetUserById(id);
         }
     }
 }
