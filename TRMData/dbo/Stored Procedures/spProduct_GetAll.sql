a924dbb1156423597937817c115d6e73bff7487a
CurtisLV <curtislv@gmail.com>
2022-11-24 12:30:19 +0200

    spProduct_GetAll

---
+++ TRMData/dbo/Stored Procedures/spProduct_GetAll.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spProduct_GetAll]
+AS
+BEGIN
+	set nocount on;
+
+	SELECT p.id
+		,p.ProductName
+		,p.[Description]
+		,p.RetailPrice
+		,p.QuantityInStock
+	FROM dbo.Product p
+	ORDER BY ProductName;
+END
