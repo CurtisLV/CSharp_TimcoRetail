10362fb054bdcffc6d84696d19ae0cc28b422289
CurtisLV <curtislv@gmail.com>
2022-11-24 13:28:35 +0200

    Display Product Data 8

---
+++ TRMDesktopUI.Library/Api/ProductEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Helpers;
+
+namespace TRMDesktopUI.Library.Api
+{
+    public class ProductEndpoint
+    {
+        private IAPIHelper _apiHelper;
+
+        public ProductEndpoint(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public List<string> GetAll()
+        {
+            //
+        }
+    }
+}
