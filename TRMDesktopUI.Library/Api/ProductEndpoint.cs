299d362abccfd0a59dc08af2d02546f3308d4d9d
CurtisLV <curtislv@gmail.com>
2022-11-24 13:47:12 +0200

    Display Product Data 9

--- TRMDesktopUI.Library/Api/ProductEndpoint.cs
+++ TRMDesktopUI.Library/Api/ProductEndpoint.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using TRMDesktopUI.Helpers;
+using TRMDesktopUI.Library.Models;
 
 namespace TRMDesktopUI.Library.Api
 {
@@ -16,9 +19,27 @@ namespace TRMDesktopUI.Library.Api
             _apiHelper = apiHelper;
         }
 
-        public List<string> GetAll()
+        public async Task<List<string>> GetAll()
         {
-            //
+            using (
+                HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/Product")
+            )
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<LoggedInUserModel>();
+                    _loggedInUser.CreatedDate = result.CreatedDate;
+                    _loggedInUser.EmailAddress = result.EmailAddress;
+                    _loggedInUser.FirstName = result.FirstName;
+                    _loggedInUser.LastName = result.LastName;
+                    _loggedInUser.Id = result.Id;
+                    _loggedInUser.Token = token;
+                }
+                else
+                {
+                    throw new Exception(response?.ReasonPhrase);
+                }
+            }
         }
     }
 }
