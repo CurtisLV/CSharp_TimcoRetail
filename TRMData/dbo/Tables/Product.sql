a0125c35d9bbb59a7475bb46d69526244f3c8dbf
CurtisLV <curtislv@gmail.com>
2022-11-26 14:50:33 +0200

    Modify SQL, API, WPF to add taxes 13

--- TRMData/dbo/Tables/Product.sql
+++ TRMData/dbo/Tables/Product.sql
@@ -1,11 +1,12 @@
-﻿CREATE TABLE [dbo].[Product]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-	[ProductName] NVARCHAR(50) NOT NULL, 
-	[Description] NVARCHAR(MAX) NOT NULL, 
-	[RetailPrice] MONEY NOT NULL,
-	[QuantityInStock] INT NOT NULL DEFAULT 1,
-	[CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-	[LastModified] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-	[IsTaxable] BIT NOT NULL DEFAULT 1,
-)
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [ProductName]     NVARCHAR (50)  NOT NULL,
+    [Description]     NVARCHAR (MAX) NOT NULL,
+    [RetailPrice]     MONEY          NOT NULL,
+    [QuantityInStock] INT            DEFAULT 1 NOT NULL,
+    [CreateDate]      DATETIME2 (7)  DEFAULT getutcdate() NOT NULL,
+    [LastModified]    DATETIME2 (7)  DEFAULT getutcdate() NOT NULL,
+    [IsTaxable]       BIT            DEFAULT 1 NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
