4a3e14f4b695ff3888e1e3f0cb2c61569125a43c
CurtisLV <curtislv@gmail.com>
2022-11-27 15:10:46 +0200

    Early refactor 2

--- TRMData/dbo/Tables/Inventory.sql
+++ TRMData/dbo/Tables/Inventory.sql
@@ -5,6 +5,6 @@
 	[Quantity] INT NOT NULL DEFAULT 1, 
 	[PurchasePrice] MONEY NOT NULL, 
 	[PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-	CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]),
+	CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id),
 
 )
