764806e151cd9524e3ecab64415fa8ce71cb366a
CurtisLV <curtislv@gmail.com>
2022-11-28 18:54:28 +0200

    Posting Sales Data to API 6

--- TRMDataManager/Controllers/SaleController.cs
+++ TRMDataManager/Controllers/SaleController.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
 
 namespace TRMDataManager.Controllers
 {
+    [Authorize]
     public class SaleController : ApiController
     {
-        //
+        //public List<ProductModel> Get()
+        //{
+        //    ProductData data = new ProductData();
+        //    return data.GetProducts();
+        //}
+
+        public void Post()
+        {
+            //
+        }
     }
 }
