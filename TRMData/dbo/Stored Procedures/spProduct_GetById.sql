c886a711fc1707cd61e516f2c4f0d8f6f6636b08
CurtisLV <curtislv@gmail.com>
2022-11-29 19:24:40 +0200

    Posting Sales Data to API 15

---
+++ TRMData/dbo/Stored Procedures/spProduct_GetById.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spProduct_GetById] @Id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT p.Id
+		,p.ProductName
+		,p.[Description]
+		,p.RetailPrice
+		,p.QuantityInStock
+		,p.IsTaxable
+	FROM dbo.Product p
+	WHERE Id = @Id;
+END
+
