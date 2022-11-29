aef4cb3caf1041f2bc08b2452b15a58cbb9c021f
CurtisLV <curtislv@gmail.com>
2022-11-29 21:00:04 +0200

    Posting Sales Data to API 25

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -65,11 +65,12 @@ namespace TRMDataManager.Library.DataAccess
 
             // Get ID from sale model
 
-            sql.LoadData<int, dynamic>(
-                "spSale_Lookup",
-                new { CashierId = sale.CashierId, SaleDate = sale.SaleDate },
-                "TRMData"
-            );
+            sale.Id = sql.LoadData<int, dynamic>(
+                    "spSale_Lookup",
+                    new { sale.CashierId, sale.SaleDate },
+                    "TRMData"
+                )
+                .FirstOrDefault();
 
             // Finish filling in the sale detail models
 
