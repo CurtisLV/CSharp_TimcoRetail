753981b99c7bb6b1db737e1c053b684a66afa823
CurtisLV <curtislv@gmail.com>
2022-12-02 21:20:48 +0200

    SQL Transactions using Dapper 13

--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -58,6 +58,8 @@ namespace TRMDataManager.Library.Internal.DataAccess
             _connection = new SqlConnection(connectionString);
             _connection.Open();
             _transaction = _connection.BeginTransaction();
+
+            isClosed = false;
         }
 
         public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
@@ -86,21 +88,40 @@ namespace TRMDataManager.Library.Internal.DataAccess
             );
         }
 
+        private bool isClosed = false;
+
         public void CommitTransaction()
         {
             _transaction?.Commit();
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void RollbackTransaction()
         {
             _transaction?.Rollback();
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void Dispose()
         {
-            CommitTransaction();
+            if (!isClosed)
+            {
+                try
+                {
+                    CommitTransaction();
+                }
+                catch
+                {
+                    // TODO: Log this issue
+                }
+            }
+
+            _transaction = null;
+            _connection = null;
         }
 
         // Open connection/start transaction method
