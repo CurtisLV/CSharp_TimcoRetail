be9ad83523b83b0a217f60c2593770473e385814
CurtisLV <curtislv@gmail.com>
2022-12-03 18:50:54 +0200

    Admin endpoints in WebAPI 6

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -94,7 +94,13 @@ namespace TRMDataManager.Library.DataAccess
 
         public List<SaleReportModel> GetSaleReport()
         {
-            //
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<SaleReportModel, dynamic>(
+                "dbo.spSale_SaleReport",
+                new { },
+                "TRMData"
+            );
         }
     }
 }
