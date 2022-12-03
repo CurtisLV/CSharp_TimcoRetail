486da667db9689685dd83b95c6da7787dc9b08a2
CurtisLV <curtislv@gmail.com>
2022-12-03 18:27:15 +0200

    Admin endpoints in WebAPI 4

---
+++ TRMData/dbo/Stored Procedures/spSale_SaleReport.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spSale_SaleReport]
+	@param1 int = 0,
+	@param2 int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	select  [s].[SaleDate], [s].[SubTotal], [s].[Tax], [s].[Total], u.FirstName, u.LastName, u.EmailAddress
+	from dbo.Sale s 
+	inner join dbo.[User] u on s.CashierId = u.Id;
+END
