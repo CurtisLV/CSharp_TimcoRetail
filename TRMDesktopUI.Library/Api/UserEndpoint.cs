fdb96d6506510e99853a9115681ad403c5320a70
CurtisLV <curtislv@gmail.com>
2022-12-05 21:26:22 +0200

    ÂDisplay users & groups/roles 7

--- TRMDesktopUI.Library/Api/UserEndpoint.cs
+++ TRMDesktopUI.Library/Api/UserEndpoint.cs
@@ -18,27 +18,24 @@ namespace TRMDesktopUI.Library.Api
             _apiHelper = apiHelper;
         }
 
-        public async Task<List<ApplicationUserModel>> GetAll()
+        public async Task<List<UserModel>> GetAll()
         {
-            //
+            using (
+                HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync(
+                    "api/User/Admin/GetAllUsers"
+                )
+            )
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<UserModel>>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
         }
-
-        //public async Task<List<ProductModel>> GetAll()
-        //{
-        //    using (
-        //        HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/Product")
-        //    )
-        //    {
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            var result = await response.Content.ReadAsAsync<List<ProductModel>>();
-        //            return result;
-        //        }
-        //        else
-        //        {
-        //            throw new Exception(response.ReasonPhrase);
-        //        }
-        //    }
-        //}
     }
 }
