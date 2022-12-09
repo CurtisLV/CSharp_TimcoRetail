d154d23f8a0827630f30888d70a48e2af3a339c1
CurtisLV <curtislv@gmail.com>
2022-12-09 18:30:10 +0200

    .net 3.1 upgrade: from webApi to asp.net core API 4

---
+++ TRMApi/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Cashier")]
+    public class ProductController : ControllerBase
+    {
+        public List<ProductModel> Get()
+        {
+            ProductData data = new ProductData();
+            return data.GetProducts();
+        }
+    }
+}
