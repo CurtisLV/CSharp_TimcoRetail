db593edff6410c50470441e509e1de313d674e33
CurtisLV <curtislv@gmail.com>
2022-12-10 20:18:52 +0200

    .net 3.1 upgrade: from webApi to asp.net core API 17

--- TRMApi/Controllers/HomeController.cs
+++ TRMApi/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TRMApi.Models;
@@ -12,10 +13,12 @@ namespace TRMApi.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, RoleManager<IdentityRole> roleManager)
         {
             _logger = logger;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -25,13 +28,25 @@ namespace TRMApi.Controllers
 
         public IActionResult Privacy()
         {
+            string[] roles = { "Admin", "Manager", "Cashier" };
+
+            foreach (var role in roles)
+            {
+                var roleExist = await
+            }
+
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                }
+            );
         }
     }
 }
