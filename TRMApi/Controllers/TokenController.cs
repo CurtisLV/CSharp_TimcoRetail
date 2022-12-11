baa58fadff712887464a795e2b0caed38e8c8ef6
CurtisLV <curtislv@gmail.com>
2022-12-11 21:56:14 +0200

    Upgrading to .net core: adding JWT Auth to api 6

--- TRMApi/Controllers/TokenController.cs
+++ TRMApi/Controllers/TokenController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TRMApi.Data;
 
 namespace TRMApi.Controllers
 {
     public class TokenController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TokenController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Route("/token")]
+        [HttpPost]
+        public async Task<IActionResult> Create(string username, string password, string grant_type)
+        {
+            //
+        }
+
+        private async Task<bool> IsValidUsernameAndPassword(string username, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(username);
+
+            return await _userManager.CheckPasswordAsync(user, password);
         }
     }
 }
