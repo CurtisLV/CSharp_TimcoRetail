b46fc5d0ffc6fea9d8c28f9daff5813a0d29be1e
CurtisLV <curtislv@gmail.com>
2022-12-22 20:43:57 +0200

    Testing Azure pipeline 3

--- TRMDesktopUI.Library/Helpers/ConfigHelper.cs
+++ TRMDesktopUI.Library/Helpers/ConfigHelper.cs
@@ -5,7 +5,7 @@ namespace TRMDesktopUI.Library.Helpers
 {
     public class ConfigHelper : IConfigHelper
     {
-        // TODO - move this from config to the API someday maybe
+        // TODO - move this from config to the API
         public decimal GetTaxRate()
         {
             string rateText = ConfigurationManager.AppSettings["taxRate"];
